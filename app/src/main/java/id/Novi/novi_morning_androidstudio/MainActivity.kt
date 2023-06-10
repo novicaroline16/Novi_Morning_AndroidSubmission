4631fef694f49e5021aa3bc2dd39ae50a600f212
novicaroline16 <carolinenovi16@gmail.com>
2023-06-10 23:22:14 +0800

    Add Project

---
+++ app/src/main/java/id/Novi/novi_morning_androidstudio/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.Novi.novi_morning_androidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
